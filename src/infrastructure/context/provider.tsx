76aab24b2849e7f2df896ad00b893bef71a525fb
Liluminalia <peloazul4@gmail.com>
2022-10-27 21:58:50 +0200

    fixing delete button

--- src/infrastructure/context/provider.tsx
+++ src/infrastructure/context/provider.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { Robot } from '../../feature/models/robot';
+import { RobotsData } from '../../feature/models/robots.data';
 import { RobotsContext } from './context';
 
 export function RobotsContextProvider({ children }: { children: JSX.Element }) {
-    const initialRobots: Array<Robot> = [];
+    const initialRobots: Array<Robot> = RobotsData;
     const [robots, setRobots] = useState(initialRobots);
 
     const handlerEraser = (deletedID: number) => {
